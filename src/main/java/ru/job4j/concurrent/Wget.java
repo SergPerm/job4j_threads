22e12d9be28d36b3a9bc615b7d509f847a61623c
Serg Mokhin <svmokhin@gmail.com>
2022-01-11 22:52:10 +0500

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,20 +1,52 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 1; i < 100; i++) {
-                            System.out.print("\rLoading " + i + " %");
-                            Thread.sleep(200);
-                        }
-                        System.out.print("\rLoaded");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
+
+/**
+ * int speed - время загрузки 1024 byte, в секундах.
+ */
+
+public class Wget implements Runnable {
+
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            Instant start = Instant.now();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                Instant finish = Instant.now();
+                long delay = speed * 1000L - Duration.between(start, finish).toMillis();
+                start = finish;
+                if (delay > 0) {
+                    Thread.sleep(delay);
                 }
-        );
-        thread.start();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
